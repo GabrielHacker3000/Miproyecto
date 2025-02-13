d8f1c81406d3789dfacf67c30d69bb2ecf2300a7
bPZNBNW2FI9sYlUMJAMrBi0fT6Umvzh675RgDemDRMY <160874621+GabrielHacker3000@users.noreply.github.com>
2025-02-13 03:27:27 +0100

    Update script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Aquí puedes agregar funciones adicionales en JavaScript si lo necesitas
+document.querySelector('.button').addEventListener('click', function() {
+  alert('¡Feliz San Valentín!');
+});
